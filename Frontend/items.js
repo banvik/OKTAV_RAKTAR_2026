dda65b0da465b1e6cc7749967e595f904aec0004
Csepei Kristóf <kristofcsepei@gmail.com>
2026-03-27 19:12:04 +0100

    formatting with prettier

--- Frontend/items.js
+++ Frontend/items.js
@@ -1,22 +1,22 @@
 export const items = [
-    {
-        name: "Léc",
-        category: "Faanyag",
-        length: 1000
-    },
-    {
-        name: "Kalapács",
-        category: "Szerszám",
-        length: 150
-    },
-    {
-        name: "Deszka",
-        category: "Faanyag",
-        length: 200
-    },
-{
-        name: "Szeg",
-        category: "Rögzítőeszköz",
-        length: 100
-    },
-]
+	{
+		name: "Léc",
+		category: "Faanyag",
+		length: 1000,
+	},
+	{
+		name: "Kalapács",
+		category: "Szerszám",
+		length: 150,
+	},
+	{
+		name: "Deszka",
+		category: "Faanyag",
+		length: 200,
+	},
+	{
+		name: "Szeg",
+		category: "Rögzítőeszköz",
+		length: 100,
+	},
+];
--- Frontend/script.js
+++ Frontend/script.js
@@ -1,90 +1,87 @@
 import { items } from "./items.js";
-const addBtn = document.getElementById("new-item-btn")
-const cancelBtn = document.getElementById("cancel-btn")
-const inventoryTable = document.getElementById("inventory-tbody")
-const itemForm = document.getElementById("item-form")
-const nameInput = document.getElementById("item-name")
-const categoryInput = document.getElementById("item-category")
-const sizeInput = document.getElementById("item-size")
-const nameTableHead = document.getElementById("th-name")
-const categortyTableHead = document.getElementById("th-category")
-const lengthTableHead = document.getElementById("th-length")
+const addBtn = document.getElementById("new-item-btn");
+const cancelBtn = document.getElementById("cancel-btn");
+const inventoryTable = document.getElementById("inventory-tbody");
+const itemForm = document.getElementById("item-form");
+const nameInput = document.getElementById("item-name");
+const categoryInput = document.getElementById("item-category");
+const sizeInput = document.getElementById("item-size");
+const nameTableHead = document.getElementById("th-name");
+const categortyTableHead = document.getElementById("th-category");
+const lengthTableHead = document.getElementById("th-length");
 
 let currentSortField = null;
 let currentSortDirection = "asc";
 
-
-
 function addRow(name, category, size) {
-    const newRow = document.createElement("tr");
+	const newRow = document.createElement("tr");
 
-    const nameCell = document.createElement("td");
-    nameCell.textContent = name;
+	const nameCell = document.createElement("td");
+	nameCell.textContent = name;
 
-    const categoryCell = document.createElement("td");
-    categoryCell.textContent = category;
+	const categoryCell = document.createElement("td");
+	categoryCell.textContent = category;
 
-    const sizeCell = document.createElement("td");
-    sizeCell.textContent = Number(size);
+	const sizeCell = document.createElement("td");
+	sizeCell.textContent = Number(size);
 
-    newRow.appendChild(nameCell);
-    newRow.appendChild(categoryCell);
-    newRow.appendChild(sizeCell);
+	newRow.appendChild(nameCell);
+	newRow.appendChild(categoryCell);
+	newRow.appendChild(sizeCell);
 
-    inventoryTable.appendChild(newRow);
+	inventoryTable.appendChild(newRow);
 }
 function renderTable() {
-    inventoryTable.innerHTML = ""
-    items.forEach(item => addRow(item.name, item.category, item.length))
+	inventoryTable.innerHTML = "";
+	items.forEach((item) => addRow(item.name, item.category, item.length));
 }
 
 function showItemForm() {
-    document.getElementById("form-container").classList.remove("hidden");
+	document.getElementById("form-container").classList.remove("hidden");
 }
 function hideItemForm() {
-    itemForm.reset()
-    document.getElementById("form-container").classList.add("hidden")
+	itemForm.reset();
+	document.getElementById("form-container").classList.add("hidden");
 }
 
 function sortBy(field) {
-    if(currentSortField === field){
-        currentSortDirection = currentSortDirection === "asc" ? "desc" : "asc"
-    }
-    else {
-        currentSortField = field
-        currentSortDirection = "asc"
-    }
-    items.sort((a,b) => {
-        const itemA = a[field]
-        const itemB = b[field]
+	if (currentSortField === field) {
+		currentSortDirection = currentSortDirection === "asc" ? "desc" : "asc";
+	} else {
+		currentSortField = field;
+		currentSortDirection = "asc";
+	}
+	items.sort((a, b) => {
+		const itemA = a[field];
+		const itemB = b[field];
 
-        let result
+		let result;
 
-        if(typeof itemA === "string"){
-            result = itemA.localeCompare(itemB)
-        }
-        else {
-            result = itemA - itemB
-        }
+		if (typeof itemA === "string") {
+			result = itemA.localeCompare(itemB);
+		} else {
+			result = itemA - itemB;
+		}
 
-        return currentSortDirection === "asc" ? result : -result
-    })
-    renderTable()
+		return currentSortDirection === "asc" ? result : -result;
+	});
+	renderTable();
 }
 
-addBtn.addEventListener("click", showItemForm)
-cancelBtn.addEventListener("click", hideItemForm)
-nameTableHead.addEventListener("click", () =>sortBy("name"))
-categortyTableHead.addEventListener("click", () =>sortBy("category"))
-lengthTableHead.addEventListener("click", () =>sortBy("length"))
-itemForm.addEventListener("submit", (e)=> {
-    e.preventDefault()
-    items.push({name: nameInput.value, category: categoryInput.value, length: sizeInput.value })
-    renderTable()
-    hideItemForm()
-})
+addBtn.addEventListener("click", showItemForm);
+cancelBtn.addEventListener("click", hideItemForm);
+nameTableHead.addEventListener("click", () => sortBy("name"));
+categortyTableHead.addEventListener("click", () => sortBy("category"));
+lengthTableHead.addEventListener("click", () => sortBy("length"));
+itemForm.addEventListener("submit", (e) => {
+	e.preventDefault();
+	items.push({
+		name: nameInput.value,
+		category: categoryInput.value,
+		length: sizeInput.value,
+	});
+	renderTable();
+	hideItemForm();
+});
 
-renderTable()
-
-
-
+renderTable();
