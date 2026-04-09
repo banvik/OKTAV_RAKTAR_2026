1f59240adb3b65c4654f8d5ffbfdc040a177f985
Csepei Kristóf <kristofcsepei@gmail.com>
2026-04-09 23:21:51 +0200

    implement warehouse logic

---
+++ Frontend/warehouses.js
@@ -0,0 +1,10 @@
+export const warehouses = [
+	{
+		name: "Budapest",
+		id: 1
+	},
+	{
+		name: "Debrecen",
+		id: 2
+	}
+];
---
+++ Frontend/whinventory.js
@@ -0,0 +1,10 @@
+
+
+export const whInventory = [
+	{ warehouseId: "1", itemName: "Léc", type:"Faáru", quantity: 20 },
+	{ warehouseId: "1", itemName: "Szeg", type:"Rögzítőeszköz", quantity: 100 },
+	{ warehouseId: "1", itemName: "Kalapács", type:"Szerszám", quantity: 5 },
+	{ warehouseId: "2", itemName: "Léc", type:"Faáru", quantity: 10 },
+	{ warehouseId: "2", itemName: "Szeg", type:"Rögzítőeszköz", quantity: 50 },
+	{ warehouseId: "2", itemName: "Kalapács", type:"Szerszám", quantity: 15 },
+];
