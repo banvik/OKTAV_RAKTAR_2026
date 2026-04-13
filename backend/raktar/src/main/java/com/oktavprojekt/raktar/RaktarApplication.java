419e51dcaaf838ccd3a60fe0a212ecabda47ebe6
banvik <viktor.karoly.ban@gmail.com>
2026-04-13 21:57:37 +0200

    product : POST végpont kész!!! / ... végpont.txt friss!!!

--- backend/raktar/src/main/java/com/oktavprojekt/raktar/RaktarApplication.java
+++ backend/raktar/src/main/java/com/oktavprojekt/raktar/RaktarApplication.java
@@ -2,11 +2,11 @@ package com.oktavprojekt.raktar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+// import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
 
 @SpringBootApplication
-// @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+//@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class RaktarApplication {
 
 	public static void main(String[] args) {
--- backend/raktar/src/main/java/com/oktavprojekt/raktar/controller/ProductController.java
+++ backend/raktar/src/main/java/com/oktavprojekt/raktar/controller/ProductController.java
@@ -8,9 +8,13 @@ import com.oktavprojekt.raktar.adatok.Product;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @CrossOrigin(origins = "*")
 @RestController
@@ -24,6 +28,13 @@ public class ProductController {
     public List<Product> getAll() {
 
         return repository.findAll(); // A Spring Boot automatikusan átalakítja JSON-ná!
+    }
+
+    @PostMapping("/products")
+    public ResponseEntity<Product> addProduct(@RequestBody Product newProduct) {
+
+        Product savedProduct = repository.save(newProduct); // A save() metódus elmenti az adatbázisba és visszaadja a már ID-val rendelkező objektumot
+        return new ResponseEntity<>(savedProduct, HttpStatus.CREATED); // 201-es kóddal válaszolunk
     }
 
 }
