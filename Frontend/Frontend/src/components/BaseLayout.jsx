223ecb69501a33c9fd5261a2691d9f1c4feb04a0
Csepei Kristóf <kristofcsepei@gmail.com>
2026-04-16 22:45:15 +0200

    tw migration

--- Frontend/Frontend/src/components/BaseLayout.jsx
+++ Frontend/Frontend/src/components/BaseLayout.jsx
@@ -2,24 +2,30 @@ import { useState } from "react";
 import { Link, Outlet } from "react-router-dom";
 
 export default function BaseLayout() {
-    const [menuOpen, setMenuOpen] = useState(false);
+	const [menuOpen, setMenuOpen] = useState(false);
+
 	return (
-		<div>
-			<nav>
+		<div className="min-h-screen bg-[#FFFBBD] text-[#153B08] font-[Montserrat,_sans-serif]">
+			<nav className="flex p-4 text-xl">
 				<button
-					className="hamburger"
+					className="text-2xl md:hidden"
 					onClick={() => setMenuOpen(!menuOpen)}
 				>
 					☰
 				</button>
-
-				<div className={menuOpen ? "nav-links open" : "nav-links"}>
+				<div
+					className={`
+						gap-4
+						md:flex md:static md:flex-row md:bg-transparent md:p-0 md:shadow-none
+						${menuOpen ? "flex flex-col absolute top-14 left-4 bg-[#EEEBAB] p-4 rounded-xl shadow" : "hidden md:flex"}
+					`}
+				>
 					<Link to="/">Termékek</Link>
 					<Link to="/warehouses">Raktár</Link>
 					<Link to="/transactions">Tranzakciók</Link>
 				</div>
 			</nav>
-			<main style={{ padding: "20px" }}>
+			<main className="p-5">
 				<Outlet />
 			</main>
 		</div>
