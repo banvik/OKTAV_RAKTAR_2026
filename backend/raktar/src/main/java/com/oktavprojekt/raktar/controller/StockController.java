0efc3b873849515dd586c0dd06c0aabd67e91ffb
banvik <viktor.karoly.ban@gmail.com>
2026-04-23 08:27:23 +0200

    delete unused import in class

--- backend/raktar/src/main/java/com/oktavprojekt/raktar/controller/StockController.java
+++ backend/raktar/src/main/java/com/oktavprojekt/raktar/controller/StockController.java
@@ -13,7 +13,6 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 @CrossOrigin(origins = "*")
