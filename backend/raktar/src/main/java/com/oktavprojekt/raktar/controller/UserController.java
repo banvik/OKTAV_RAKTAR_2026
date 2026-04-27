2df47130d1ca2211a705a794bd9812dca0072e08
banvik <viktor.karoly.ban@gmail.com>
2026-04-27 22:33:48 +0200

    controller cleaning

--- backend/raktar/src/main/java/com/oktavprojekt/raktar/controller/UserController.java
+++ backend/raktar/src/main/java/com/oktavprojekt/raktar/controller/UserController.java
@@ -4,6 +4,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.oktavprojekt.raktar.adatbaziskezelo.UserRepository;
 import com.oktavprojekt.raktar.adatok.User;
 
+import lombok.Builder;
+import lombok.Data;
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,9 +22,27 @@ public class UserController {
     @Autowired
     private UserRepository repository;
 
+// ----  DTO segédosztályok  ----
+
+    @Data
+    public static class LoginRequest {
+        private String userId;
+        private String password;
+    }
+
+    @Data
+    @Builder
+    public static class LoginResponse {
+        private Integer userId;
+        private String roleName;
+        private String message;
+    }
+
     @GetMapping("/users")
     public List<User> getAll() {
 
         return repository.findAll(); // Spring automatically converts to JSON
     }
+
+
 }
