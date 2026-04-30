1dd9cc1a40c0dbd27b8592d69b0d9b776c3f4f7c
banvik <viktor.karoly.ban@gmail.com>
2026-04-30 23:59:33 +0200

    database cleaning

--- backend/users.sql
+++ backend/users.sql
@@ -3,6 +3,6 @@ USE `oktav_raktarprojekt_02`;
 -- 1. Felhasználók feltöltése
 INSERT INTO `users` (username, full_name, password_hash, role_name) VALUES
 ('kristof', 'Csepei Kristóf', 'password_123', 'admin'),
-('miki', 'Pap Miklós', 'password_345', 'beszerző'),
+('miki', 'Pap Miklós', 'password_345', 'raktáros'),
 ('viktor', 'Bán Viktor', 'password_567', 'raktárvezető'),
-('robi', 'Raktáros Róbert', 'password_789', 'raktáros');
+
